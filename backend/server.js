67c8117570c33a6cf068c9cc2ba9b1b0eb56ee24
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-05-31 19:00:05 +0200

    Added starter code to server.js

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,17 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`)
+})
