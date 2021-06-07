b48016d1688e98aaa17f47e3476bb01cf4770484
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-06-07 13:13:03 +0200

    Added recipe to recipes.json and changed from array to string on instructions

---
+++ frontend/src/components/Searchbar.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+import styled from 'styled-components/macro'
+
+const Searchbar = () => {
+  return (
+    <>
+      <Form>
+        <Input 
+          id="search-field"
+          type="text"
+          placeholder="fritext"
+          // value={searchField}
+        /> 
+        <Select id="type">
+          <option disables>typ</option>
+          <option value="frukost">Frukost</option>
+          <option value="lunchMiddag">Lunch/middag</option>
+          <option value="fika">Fika</option>
+          <option value="dryck">Dryck</option>
+        </Select>
+        <TagsContainer>
+          <TagBtn 
+            id="tags"
+            value="veg">
+              veg.
+          </TagBtn>
+          <TagBtn 
+            id="tags"
+            value="glutenfri">
+              glutenfri
+          </TagBtn>
+          <TagBtn 
+            id="tags"
+            value="snabbt">
+              snabbt
+          </TagBtn>
+          <TagBtn 
+            id="tags"
+            value="eldKok">
+              eld/kök
+          </TagBtn>
+        </TagsContainer>
+        <SubmitButton type="submit">sök</SubmitButton>
+      </Form>
+    </>
+  )
+}
+
+export default Searchbar
+
+const Form = styled.form`
+  background-color: pink;
+  width: 80%;
+  margin: 20px auto;
+  padding: 15px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const TagsContainer = styled.div`
+  display: flex;  
+  flex-direction: row;
+`
+const Input = styled.input`
+  border: 1px solid #CCC;
+  margin: 0;
+  padding: 10px;
+  width: 200px;
+  height: 40px;
+  font-size: 16px;
+`
+const Select = styled.select`
+  border: 1px solid #CCC;
+  margin: 0;
+  padding: 10px;
+  width: 150px;
+  height: 40px;
+  font-size: 16px;
+`
+const TagBtn = styled.button`
+  cursor: pointer;
+  border: 1px solid #CCC;
+  margin: 0 5px;
+  font-size: 14px;
+  color: #000;
+  background: #FFF;
+  padding: 0 10px;
+  height: 40px;
+
+  &active {
+
+  }
+`
+const SubmitButton = styled(TagBtn)`
+  background-color: lavender;
+`
