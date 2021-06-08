f09930ad4dd9b9d148bb0cfb1738d052acf30f28
Aepera <astridepersson@gmail.com>
2021-06-08 14:42:18 +0200

    updated footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
 const Footer = () => {
   return (
-    <Background>This is a footer</Background>
+    <Background>
+      <FooterWrapper>
+        <Header>Friluftsmat</Header>
+        <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </Paragraph>
+        <Link to="/kontakt" ><Button>Kontakta oss</Button></Link>
+        <p>&copy; Astrid Persson & Julia Ros 2021</p>
+      </FooterWrapper>
+    </Background>
   )
 }
 
@@ -13,6 +20,36 @@ export default Footer
 const Background = styled.div`
   background-color: #999;
   width: 100%;
-  height: 100px;
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  align-items: center;
+  margin: 50px 0 20px;
+`
+const Header = styled.h1`
+`
+const Paragraph = styled.p`
+  text-align: center;
+  margin-bottom: 50px;
+`
+const Button = styled.button`
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 17px;
+  color: #fff;
+  padding: 10px 12px;
+  border: 1px solid black;
+  border-radius: 5px;
+  background-color: #000;
+  cursor: pointer;
+  margin-bottom: 50px;
+  &:hover {
+    transition: all .5s ease-in-out;
+    background-color: #fff;
+    color: silver;
+  }
 `
