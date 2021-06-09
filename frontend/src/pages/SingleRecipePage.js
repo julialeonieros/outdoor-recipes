1423a31a03dde23ce4118258c1bde347ac2ed6df
Aepera <astridepersson@gmail.com>
2021-06-09 17:11:36 +0200

    singleRecipePage displays singlerecipe

--- frontend/src/pages/SingleRecipePage.js
+++ frontend/src/pages/SingleRecipePage.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components/macro'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 import HeaderSmall from '../components/HeaderSmall'
 // import { API_URL } from '../reusables/urls'
-// import SingleRecipe from '../components/RecipeCard'
+
 
 const SingleRecipePage = () => {
 
@@ -18,21 +18,38 @@ const SingleRecipePage = () => {
   useEffect(() => {
     fetch(API_RECIPE_URL)
     .then(res => res.json())
-    .then(data => setSingleRecipe(data))
+    .then(data => {
+      setSingleRecipe(data)
+    })
   }, [API_RECIPE_URL])
 
-  console.log(singleRecipe)
+  const ingredients = singleRecipe.ingredients
+  const instructions = singleRecipe.instructions
+
+  //console.log('row24',singleRecipe.instructions.split('.'))
 
   return (
     <>
       <HeaderSmall />
       <RecipeWrapper>
-        <>
-        <IMG 
+        <InnerWrapper>
+          <IMG 
           src={singleRecipe.image}
           alt={singleRecipe.title}
-        />
-        </>
+          />
+          <h1>{singleRecipe.title}</h1>
+          <IngredientsWrapper>
+            {ingredients && ingredients.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
+          </IngredientsWrapper>
+          <InstructionsWrapper>
+            {instructions && instructions.split('.').map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+          </InstructionsWrapper>
+          <Link to="/" style={{ textDecoration: 'none', color: '#000', fontSize: '20px'}}><BACK>⏪ TILLBAKA</BACK></Link>
+        </InnerWrapper>
       </RecipeWrapper>
     </>
   )
@@ -41,13 +58,27 @@ const SingleRecipePage = () => {
 export default SingleRecipePage
 
 const RecipeWrapper = styled.div`
-  border: solid purple 3px;
   display: flex;
-  flex-flow: row-wrap;
+  justify-content: center;
 `
+const InnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: auto 0;
+`
+const IngredientsWrapper = styled.div`
+  margin: 20px 0;
+`
+const InstructionsWrapper = styled.div`
+  margin: 20px 0;
+`
+
 const IMG = styled.img`
-  width: 200px;
-  height: 200px;
+  width: 100%;
+  ${'' /* height: 200px; */}
   object-fit: cover;
   border: solid black 1px;
+`
+const BACK = styled.p`
 `
