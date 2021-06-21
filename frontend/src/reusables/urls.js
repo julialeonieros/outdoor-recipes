71c5d8492438f6240381a2c4be37085936658725
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-06-21 18:35:15 +0200

    changes to .env

--- frontend/src/reusables/urls.js
+++ frontend/src/reusables/urls.js
@@ -1,3 +1,2 @@
 // export const API_URL = 'http://localhost:8080/recipes'
 export const API_URL = 'https://outdoor-recipes.herokuapp.com/recipes'
-//export const API_URL_SINGLE_RECIPE = `https://outdoor-recipes.herokuapp.com/recipes/${id}`
